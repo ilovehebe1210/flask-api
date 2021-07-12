389b28af39015d7305cc10beacac6db2ea26deed
ilovehebe1210 <87172699+ilovehebe1210@users.noreply.github.com>
2021-07-12 22:22:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,13 +4,6 @@ from app import app
 
 import pickle
 
-pickle_in = open(r'apps/model/randomforest.pickle', 'rb')
-forest = pickle.load(pickle_in)
-
-def predict(input):
-    pred = forest.predict(input)[0]
-    print(pred)
-    return pred
 
 
 
