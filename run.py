eef372b0a9783862195682fbf6402a1a17f39562
ilovehebe1210 <87172699+ilovehebe1210@users.noreply.github.com>
2021-07-12 22:09:48 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,18 @@
 
 from app import app
 
+import pickle
+
+pickle_in = open(r'apps/model/randomforest.pickle', 'rb')
+forest = pickle.load(pickle_in)
+
+def predict(input):
+    pred = forest.predict(input)[0]
+    print(pred)
+    return pred
+
+
+
 @app.route('/')
 def index():
     return 'Flask API started'
